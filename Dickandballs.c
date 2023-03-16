95836658566c733c4e97cf14cbfd490e26dd8350
Tim Rosendahl <97912162+Limpowitch@users.noreply.github.com>
2023-03-16 13:21:24 +0100

    reee

--- Dickandballs.c
+++ Dickandballs.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool>
 #include <termios.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -13,13 +12,13 @@
 
 
 //filedirectory
-int filedescriptor;
+int filedirector;
 // ints for the lights, either on or off (1 = green, 0 = red)
 int LightNorth;
 int LightSouth;
 
 // amount of cars in the queues, and on the bridge at the start
-int Northqueue, Southqueue; Bridgecount = 0;
+int Northqueue, Southqueue, Bridgecount = 0;
 
 // treads
 pthread_t pr;
@@ -31,8 +30,8 @@ pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
 int Open_port(){
 	int speed = 9600; //serial port speed of 9600bps
-	int filedescriptor = open("/dev/ttyS0", O_RDWR);
-	if (filedescriptor < 0){
+	int filedirector = open("/dev/ttyS0", O_RDWR);
+	if (filedirector < 0){
 		printf("yikes \n");
 	}
 	struct termios termios_p; //Get and set terminal attributes
@@ -44,8 +43,8 @@ int Open_port(){
 	//tcflag_t   c_lflag;        /* local modes */
 	//cc_t       c_cc[NCCS];     /* control chars */
 	
-	if (tcgetattr(filedescriptor, &termios_p)){
-		printf("Terminos attributes couldn't load")
+	if (tcgetattr(filedirector, &termios_p)){
+		printf("Terminos attributes couldn't load");
 		return -1;
 	}
 	
@@ -53,70 +52,59 @@ int Open_port(){
 	
 	cfsetispeed(&termios_p, (speed_t)speed);
 
-	tty.c_cflag |= CLOCAL | CREAD;
-	tty.c_cflag &= ~CSIZE;
-	tty.c_cflag |= CS8;
-	tty.c_cflag &= ~PARENB;
-	tty.c_cflag &= ~CSTOPB;
-	tty.c_cflag &= ~CRTSCTS;
-	tty.c_lflag &= ~ICANON;
-	tty.c_lflag &= ~ISIG;
-	tty.c_lflag &= ~(ECHO | ECHOE | ECHONL | IEXTEN);
-	tty.c_iflag &= ~IGNCR;
-	tty.c_iflag &= ~INPCK;
-	tty.c_iflag &= ~(INLCR | ICRNL | IUCLC | IMAXBEL);
-	tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-	tty.c_oflag &= ~OPOST;
-	tty.c_cc[VEOL] = 0;
-	tty.c_cc[VEOL2] = 0;
-	tty.c_cc[VEOF] = 0x04;
+	termios_p.c_cflag |= CLOCAL | CREAD;
+	termios_p.c_cflag &= ~CSIZE;
+	termios_p.c_cflag |= CS8;
+	termios_p.c_cflag &= ~PARENB;
+	termios_p.c_cflag &= ~CSTOPB;
+	termios_p.c_cflag &= ~CRTSCTS;
+	termios_p.c_lflag &= ~ICANON;
+	termios_p.c_lflag &= ~ISIG;
+	termios_p.c_lflag &= ~(ECHO | ECHOE | ECHONL | IEXTEN);
+	termios_p.c_iflag &= ~IGNCR;
+	termios_p.c_iflag &= ~INPCK;
+	termios_p.c_iflag &= ~(INLCR | ICRNL | IUCLC | IMAXBEL);
+	termios_p.c_iflag &= ~(IXON | IXOFF | IXANY);
+	termios_p.c_oflag &= ~OPOST;
+	termios_p.c_cc[VEOL] = 0;
+	termios_p.c_cc[VEOL2] = 0;
+	termios_p.c_cc[VEOF] = 0x04;
 	
-	if(tcsetattr(filedescriptor, TCSANOW, &termios_p)){
-		printf(sterror(errno));
+	if(tcsetattr(filedirector, TCSANOW, &termios_p)){
+		printf(strerror(errno));
 		return -1;
 	}
 	
-	return filedescriptor;
+	return filedirector;
 	
 }
 
-void *portreader(void *arg){
-	uint8_t buf;
-	while(true){
-		int bytereader = read(filedescriptor, &buf, sizeof(buf));
-		if(bytereader != 0){
-			LightNorth = (buf & 1) //om grönt ljus om 1, rött om 0 på least signifigant bit
-			LightSouth = (c >> 2) & 1 //bitshift med 2, om 3e biten är 1 så grönt, rött om 0 på least signifigant bit
-			pthread_create(&ui, NULL, gui, NULL);
-		}
-	}
 
-}
 
 void *GUI(void *arg){
 	pthread_mutex_lock(&m);
-	printf("\033c\n"); //clearar screenen i cygwin
+	//printf("\033c\n"); //clearar screenen i cygwin
 	if(LightNorth){
 		printf(
-			"North: ", "\U0001F7E2", "\n"
-		)
+			"North: 1 \n", "\U0001F7E2", "\n"
+		);
 	}
-	else(LightNorth){
+	else;{
 		printf(
-			"North: ", "\U0001F534", "\n"
-		)
-	}
+			"North: 2 \n", "\U0001F534", "\n"
+		);
+	};
 	//northqueue
 	if(LightSouth){
 		printf(
-			"North: ", "\U0001F7E2", "\n"
-		)
+			"South: 1 \n", "\U0001F7E2", "\n"
+		);
 	}
-	else(LightSouth){
+	else;{
 		printf(
-			"North: ", "\U0001F534", "\n"
-		)
-	}
+			"South: 2 \n", "\U0001F534", "\n"
+		);
+	};
 	//southqueue
 	//bridgecount
 	pthread_mutex_unlock(&m);
@@ -124,9 +112,25 @@ void *GUI(void *arg){
 
 }
 
-Input(void *arg){
+void *portreader(void *arg){
+	uint8_t buf;
+	
+	while(1){
+		uint8_t inverted_buf = ~buf;
+		int bytereader = read(filedirector, &inverted_buf, sizeof(inverted_buf));
+		
+		if(bytereader != 0){
+			LightNorth = (inverted_buf & 1); //om grönt ljus om 1, rött om 0 på least signifigant bit
+			LightSouth = (inverted_buf >> 2) & 1; //bitshift med 2, om 3e biten är 1 så grönt, rött om 0 på least signifigant bit
+			pthread_create(&ui, NULL, GUI, NULL);
+		}
+	}
+
+}
+
+int Input(void *arg){
 	char keycharacter;
-	while(true){
+	while(1){
 		keycharacter = getchar();
 		switch (keycharacter)
 		{
@@ -141,27 +145,28 @@ Input(void *arg){
 		case 'e':
 			break;
 		default:
-			printf("input a valid key you donkey")
+			printf("input a valid key you donkey");
 		}
 	}
 }
 
 void portwriter(uint8_t input){
-	int writtenbytes = write(filedescriptor, &input, 1);
+	int writtenbytes = write(filedirector, &input, 1);
 	pthread_create(&ui, NULL, GUI, NULL);
-	if (filedescriptor < 0){
+	if (filedirector < 0){
 		printf("error writing to port in portwriter \n");
 	}
 }
 
 int main(void){
-	filedescriptor = Open_port();
+ 	printf("Välkommen till mitt ascoola program \n");
+	filedirector = Open_port();
 
 	if(pthread_create(&pr, NULL, portreader, NULL)){
-		printf("Threadfail on: Portreader")
+		printf("Threadfail on: Portreader");
 	}
 	if(pthread_create(&ui, NULL, GUI, NULL)){
-		printf("Threadfail on: GUI / Input")
+		printf("Threadfail on: GUI / Input");
 	}
 	
 
